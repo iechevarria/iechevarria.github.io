1a3bec6b559016d2e094e9a248c706cb3f47429d
Ivan Echevarria <iechevarria@email.wm.edu>
2017-10-06 12:36:55 -0400

    add gallery selection

--- photo.js
+++ photo.js
@@ -3,21 +3,30 @@ var wrap = d.querySelector('.wrap')
 var items = d.querySelector('.items')
 var itemCount = d.querySelectorAll('.item').length
 var scroller = d.querySelector('.scroller')
+var galleries = d.querySelector('.galleries')
 var pos = 0
 var transform = Modernizr.prefixed('transform')
 
-function setTransform() {
+function setTransform () {
   items.style[transform] = 'translate3d(' + (-pos * items.offsetWidth) + 'px,0,0)'
 }
 
-function prev() {
+function prev () {
   pos = (pos - 1 + itemCount) % itemCount
   setTransform()
 }
 
-function next() {
+function next () {
   pos = (pos + 1) % itemCount
   setTransform()
+}
+
+function showGalleries () {
+  if (galleries.style.display === 'none') {
+    galleries.style.display = 'block'
+  } else {
+    galleries.style.display = 'none'
+  }
 }
 
 window.addEventListener('resize', setTransform)
