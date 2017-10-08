de0566aa4a19df44f3729a965b0c5bc41c0c1977
Ivan Echevarria <iechevarria@email.wm.edu>
2017-10-08 13:18:44 -0400

    update site content

---
+++ default.js
@@ -0,0 +1,12 @@
+var d = document
+var galleries = d.querySelector('.galleries')
+
+function showGalleries () {
+  if (galleries.style.display === 'none') {
+    galleries.style.display = 'block'
+  } else {
+    galleries.style.display = 'none'
+  }
+}
+
+showGalleries()
